726ec78dbeba4117570e42706c14fca3bab6dcda
Andrew Smith <andrew@andrew.codes>
2019-03-03 15:24:41 -0500

    properly ignore files from test coverage calculation

--- wallaby.js
+++ wallaby.js
@@ -6,6 +6,10 @@ module.exports = function() {
       { pattern: 'packages/**/node_modules/**', ignore: true },
     ],
     tests: ['packages/**/*.test.js'],
+    filesWithNoCoverageCalculated: [
+      'packages/**/testing/**/*.js',
+      'packages/webhooked-plugin-example/**/*.js',
+    ],
     env: {
       type: 'node',
       runner: 'node',
